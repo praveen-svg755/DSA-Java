dfce641711b1a0458c04ffcbcbcbe805480912cb
praveen-svg755 <thiruveedipraveen97@gmail.com>
2025-07-06 10:59:08 +0530

    first commit

---
+++ src/com/praveen/mathemathics/countDigits.java
@@ -0,0 +1,11 @@
+package com.praveen.mathemathics;
+
+public class countDigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
